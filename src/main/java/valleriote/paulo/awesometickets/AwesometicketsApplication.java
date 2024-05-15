2d32f1915164c2b4ad70bb9ccf6aafad10136003
Paulo Valleriote <pmv@pop-os.www.roteador.multilaser.com>
2024-05-15 20:31:51 -0300

    iniciando projeto

---
+++ src/main/java/valleriote/paulo/awesometickets/AwesometicketsApplication.java
@@ -0,0 +1,13 @@
+package valleriote.paulo.awesometickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesometicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesometicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/valleriote/paulo/awesometickets/AwesometicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package valleriote.paulo.awesometickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesometicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
