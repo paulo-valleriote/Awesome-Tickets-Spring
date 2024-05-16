5c4f73f34a81898d35c8d88fc0078a86ee6f7847
Paulo Valleriote <pmv@pop-os.www.roteador.multilaser.com>
2024-05-15 22:28:49 -0300

    implementando serviço de autenticação do usuario

--- src/main/java/valleriote/paulo/awesometickets/infra/security/auth/AuthService.java
+++ src/main/java/valleriote/paulo/awesometickets/infra/security/auth/AuthService.java
@@ -5,13 +5,30 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import valleriote.paulo.awesometickets.app.handler.exceptions.user.UserNotFound;
+import valleriote.paulo.awesometickets.domain.entity.User;
+import valleriote.paulo.awesometickets.domain.repository.UserRepository;
+import valleriote.paulo.awesometickets.infra.persistance.repository.UserRepositoryImplementation;
+
+import java.util.Optional;
 
 @Service
 @Log4j2
 public class AuthService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepositoryImplementation userRepositoryImplementation) {
+        this.userRepository = userRepositoryImplementation;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // IMPLEMENTAR
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UserNotFound {
+        Optional<User> user = userRepository.findByUsername(username);
+
+        if (user.isEmpty()) {
+            throw new UserNotFound();
+        }
+
+        return user.get();
     }
 }
