7f41960d94b2d012f387d48c8dc5065792958c38
Paulo Valleriote <paulo@suaid.io>
2024-07-03 13:11:07 -0300

    Criando interface eventos

--- src/main/java/valleriote/paulo/awesometickets/domain/service/event/EventService.java
+++ src/main/java/valleriote/paulo/awesometickets/domain/service/event/EventService.java
@@ -1,15 +1,17 @@
 package valleriote.paulo.awesometickets.domain.service.event;
 
 import valleriote.paulo.awesometickets.app.dto.event.EventCreateDTO;
+import valleriote.paulo.awesometickets.app.dto.event.EventResponseDTO;
 import valleriote.paulo.awesometickets.app.dto.event.EventUpdateDTO;
 import valleriote.paulo.awesometickets.domain.entity.Event;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EventService {
-    Event findById(String id);
-    List<Event> list();
-    Event create(EventCreateDTO dto);
+    EventResponseDTO findById(String id);
+    List<EventResponseDTO> list();
+    EventResponseDTO create(EventCreateDTO dto);
     void update(String id, EventUpdateDTO dto);
     void delete(String id);
 }
